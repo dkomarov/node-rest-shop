29aa40286f58cbb8c96a74220f13d24f14532e9d
dkomarov <dkomarov@hawk.iit.edu>
2020-06-07 13:17:26 -0500

    require all new modules
    body-parser, morgan & mongoose

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const express = require('express')
+const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');

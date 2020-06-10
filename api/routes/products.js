fe92519f8f4884c6efdde2f1864878ee89810bd3
dkomarov <dkomarov@hawk.iit.edu>
2020-06-10 14:06:26 -0500

    create storage, filter, & upload constants
    used to process each file upload (from the multi-part form data input) using multer

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname)
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true); // will save file
+  } else {
+    cb(null, false); // will not save file
+  }
+};
+
+const upload = multer({
+  storage: storage, 
+  limits: {
+  fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
+});
 
 const Product = require('../models/product_model')
 
